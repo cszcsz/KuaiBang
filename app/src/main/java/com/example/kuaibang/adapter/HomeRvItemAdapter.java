cef2001c91489376a1eab601d8d8fb3b592ace78
caisongzhi <175837951@qq.com>
2018-08-18 18:29:42 +0800

    8.18提交： 完成主页recycleView下拉加载和上拉刷新功能

--- app/src/main/java/com/example/kuaibang/adapter/HomeRvItemAdapter.java
+++ app/src/main/java/com/example/kuaibang/adapter/HomeRvItemAdapter.java
@@ -17,10 +17,13 @@ public class HomeRvItemAdapter extends BaseQuickAdapter<Test,BaseViewHolder> {
 
     @Override
     protected void convert(BaseViewHolder helper, Test item) {
+        helper.addOnClickListener(R.id.home_rv_item_userImg);   //点击用户头像事件
+
+// TODO: 通过后台获取实体对象后，在此将实体数据显示到item上
 //        helper.setText(R.id.home_rv_item_title,item.getTitle())
 //                .setText(R.id.home_rv_item_content,item.getContent())
 //                .setImageResource(R.id.home_rv_item_img,R.mipmap.ic_launcher);
-        // Glide使用示例
-    //  Glide.with(mContext).load(item.getUserAvatar()).crossFade().into((ImageView) viewHolder.getView(R.id.iv));
+//        Glide使用示例
+//        Glide.with(mContext).load(item.getUserAvatar()).crossFade().into((ImageView) viewHolder.getView(R.id.iv));
     }
 }
--- app/src/main/java/com/example/kuaibang/fragment/HomeMainFragment.java
+++ app/src/main/java/com/example/kuaibang/fragment/HomeMainFragment.java
@@ -1,15 +1,20 @@
 package com.example.kuaibang.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.ajguan.library.EasyRefreshLayout;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.kuaibang.R;
 import com.example.kuaibang.adapter.HomeRvItemAdapter;
 import com.example.kuaibang.entity.Test;
@@ -20,6 +25,7 @@ import java.util.List;
 public class HomeMainFragment extends Fragment {
 
     private RecyclerView recyclerView;
+    private EasyRefreshLayout easyRefreshLayout;
     private List<Test> datas;
     private HomeRvItemAdapter adapter;
 
@@ -31,11 +37,14 @@ public class HomeMainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.home_main_fg,container,false);
         recyclerView = view.findViewById(R.id.home_recycle_view);
+        easyRefreshLayout = view.findViewById(R.id.home_refresh_layout);
         datas = new ArrayList<>();
         Test testData;
-        for (int i = 0; i < 15; i++) {
+        int i;
+        for (i = 0; i < 5; i++) {
             testData = new Test();
             testData.setTitle("我是第" + i + "条标题");
             testData.setContent("第" + i + "条内容");
@@ -46,6 +55,66 @@ public class HomeMainFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
 
         adapter = new HomeRvItemAdapter(R.layout.home_rv_item,datas);
+
+        // 为rv中的每个item条目设置点击事件
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                Toast.makeText(getContext(),"你点击了第"+(position+1)+"条求助帖",Toast.LENGTH_SHORT).show();
+            }
+        });
+        // 为rv中每个item条目下面的子控件设置点击事件
+        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                Snackbar.make(view,"你点击了第"+(position+1)+"位用户头像",Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // 为recycleView设置下拉加载，上拉刷新功能，PS:若需要，可以自定义加载和刷新的UI界面
+        easyRefreshLayout.addEasyEvent(new EasyRefreshLayout.EasyEvent() {
+            @Override
+            public void onLoadMore() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        final List<Test> moreDatas = new ArrayList<>();
+                        Test moreData;
+                        for (int i=0;i<5;i++){
+                            moreData = new Test();
+                            moreData.setTitle("我是第"+i+"条标题");
+                            moreData.setContent("第" + i + "条内容");
+                            moreDatas.add(moreData);
+                        }
+                        adapter.getData().addAll(moreDatas);
+                        adapter.notifyDataSetChanged();
+                        easyRefreshLayout.loadMoreComplete();
+                    }
+                },1000);
+            }
+
+            @Override
+            public void onRefreshing() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<Test> newDatas = new ArrayList<>();
+                        Test newData;
+                        for (int i=0;i<5;i++){
+                            newData = new Test();
+                            newData.setTitle("哈哈，我是新的第"+i+"条标题");
+                            newData.setContent("呵呵，我是新的第" + i + "条内容");
+                            newDatas.add(newData);
+                        }
+                        adapter.setNewData(newDatas);
+                        easyRefreshLayout.refreshComplete();
+                        Toast.makeText(getContext(),"refresh success!",Toast.LENGTH_SHORT).show();
+                    }
+                },1000);
+            }
+        });
+
         adapter.openLoadAnimation();
         recyclerView.setAdapter(adapter);
 
