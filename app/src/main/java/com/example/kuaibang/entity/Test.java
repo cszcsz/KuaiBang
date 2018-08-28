ca4b17e472df62793a9eab3d01e60e3c789abd1a
caisongzhi <175837951@qq.com>
2018-08-28 18:28:17 +0800

    8.28提交：完成我的求助首页

--- app/src/main/java/com/example/kuaibang/entity/Test.java
+++ app/src/main/java/com/example/kuaibang/entity/Test.java
@@ -1,12 +1,20 @@
 package com.example.kuaibang.entity;
 
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+
+
 import cn.bmob.v3.BmobObject;
 
-public class Test extends BmobObject {
+public class Test extends BmobObject{
+
+    public static final int HELPS_STATE = 1;    // 待帮助状态
+    public static final int HELPING_STATE = 2;  // 正在帮助状态
+    public static final int HELPED_STATE = 3;   // 已帮助状态
 
     private String title;
     private String content;
     private String imgUrl;
+    private int type;
 
     public String getTitle() {
         return title;
@@ -30,5 +38,13 @@ public class Test extends BmobObject {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 }
